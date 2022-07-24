6e9d01b17e8620e29029c276a1f8ba24bfde1134
eddydevops <eddypsc@gmail.com>
2022-07-24 15:44:51 -0400

    project setup

---
+++ app.js
@@ -0,0 +1,39 @@
+const tableBody = document= getElementById('table-body');
+
+let flight = [
+    {
+        time: "08:11",
+        destination: "OMAN",
+        flight: "QR 0778",
+        gate: "A 01",
+        remarks: "ON TIME"  
+    },
+    {
+        time: "12:39",
+        destination: "LONDON",
+        flight: "CL 320",
+        gate: "C 31",
+        remarks: "CANCELLED" 
+    },
+    {
+        time: "13:21",
+        destination: "DUBAI",
+        flight: "DXB 201",
+        gate: "A 019",
+        remarks: "CANELLED"
+    },
+    {
+        time: "14:01",
+        destination: "FRANKFURT",
+        flight: "FR 402",
+        gate: "B 02",
+        remarks: "ON TIME"
+    },
+    {
+        time: "15:22",
+        destination: "TOKYO",
+        flight: "TK 211",
+        gate: "A 32",
+        remarks: "DELAYED"
+    }
+];
