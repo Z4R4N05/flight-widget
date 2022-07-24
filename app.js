886a243dfa075a1c1525c6e3098a5cb70091fd71
eddydevops <eddypsc@gmail.com>
2022-07-24 18:16:36 -0400

    adding css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const tableBody = document= getElementById('table-body');
+const tableBody = document.getElementById('table-body');
 
 let flight = [
     {
@@ -36,4 +36,85 @@ let flight = [
         gate: "A 32",
         remarks: "DELAYED"
     }
-];
+]
+
+const destination = ["TOKYO", "FRANKFURT", "DUBAI", "LONDON", "OMAN", "BEIRUT"]
+const remarks = ["ON TIME", "DELAYED", "CANCELLED"] 
+let hour = 15 
+
+function populateTable() {
+    for (const flight of flights) {
+        const tableRow = document.createElement("tr") 
+         
+        for( const flightDetail in flight) {
+            const tableCell = document.createElement("td")
+            const word = Array.from(flight[flightDetail])
+
+            for(const [index, letter] of word.entries()) {
+                const letterElement = document.createElement('div')
+
+                setTimeout(() => {
+                    letterElement.classList.add('flip')
+                    letterElement.textContent = letter
+                    tableCell.append(letter)
+
+                }, 100 * index)
+
+            }
+
+            tableRow.append(tableCell) 
+        }
+
+        tableBody.append(tableRow)
+    }
+}
+
+populateTable() 
+
+function generateRamdomLetter() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    return alphabet.charAt(Math.floor(Math.ramdom() * alphabet.length))
+}
+
+function generateRamdomNumbre(maxNumber) {
+    const numbers = "0123456789"
+    if (maxNumber) {
+        const newNumbers = numbers.slice(0, maxNumber + 1) 
+        return newNumbers.charAt(Math.floor(Math.ramdom() * newNumbers.length))
+    }
+    return numbers.charAt(Math.floor(Math.ramdom() * numbers.length))
+}
+
+function generateTime() {
+    let displayHour = hour
+
+    if(hour < 24) {
+        hour++
+    }
+    if (hour >= 24) {
+        hour = 1
+        displayHour = hour
+    }
+    if (hour < 10) {
+        displayHour = "0" + hour
+    }
+
+    return displayHour + ":" + generateRamdomNumber() + generateRamdomNumber()
+}
+
+function shuffleUp () {
+    flights.shift()
+    flights.push({
+        time: generateTime(),
+        destination: destinations[Math.floor(Math.ramdom() * destinations.length)],
+        flight: generateRamdomLetter() + generateRamdomLetter() + " " + generateRamdomNumber() + generateRamdomNumber(),
+        gate: generateRamdomLetter() + " " + generateRamdomNumber() + generateRamdomNumber(), 
+        remarks: remarks[Math.floor(Math.ramdom() * remarks.length)]
+    }) 
+    tableBody.textContent = ""
+    populateTable()
+
+}
+
+setInterval(shuffleUp, 5000) 
+
